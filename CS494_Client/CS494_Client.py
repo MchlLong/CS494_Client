72575af0bde18cfe605664d364da5579df690321
Michael Long <michael.long.code@gmail.com>
2019-05-30 10:54:44 -0700

    Mchl -- Updated GUI, centered relative to size of screen

--- CS494_Client/CS494_Client.py
+++ CS494_Client/CS494_Client.py
@@ -1,6 +1,9 @@
 # Michael Long, Gennadii Sytov -- CS494 -- Client Main -- May 2019
 
+import client_gui as gui
+
 def main():
+    screen = gui.gui_controller()
     pass
 
 if __name__ == '__main__':
