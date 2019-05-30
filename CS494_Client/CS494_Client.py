e4d5d41bb4f2c3052d5f4524e36b7ad0ea4aa74c
MchlLong <michael.long.code@gmail.com>
2019-05-29 23:07:18 -0700

    Add project files.

---
+++ CS494_Client/CS494_Client.py
@@ -0,0 +1,7 @@
+# Michael Long, Gennadii Sytov -- CS494 -- Client Main -- May 2019
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ CS494_Client/client_gui.py
@@ -0,0 +1,7 @@
+# Michael Long, Gennadii Sytov -- CS494 -- Client Main -- May 2019
+
+class gui():
+
+    def __init__():
+        pass
+
